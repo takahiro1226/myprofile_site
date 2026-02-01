d31e6de73fa5068cf5c5b1ba3b26ca67e45eafab
takahiro1226 <arzu.ruguz.beorc.night@gmail.com>
2026-02-01 22:06:42 +0900

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_account_id" {
+  type        = string
+  description = "AWS Account ID"
+}
+
+variable "project_name" {
+  description = "profile_site"
+  type        = string
+}
+
+variable "environment" {
+  description = "production"
+  type        = string
+}
+
